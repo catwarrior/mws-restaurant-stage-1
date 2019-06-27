5aa17dff954c0f7dd48efb5dcf30531c8ee27844
shufanli <shufan.li@daimler.com>
2019-06-27 13:53:41 +0800

    update service worker.

--- sw.js
+++ sw.js
@@ -1,16 +1,19 @@
-console.log("hello");
 self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open('cache-v1').then(function (cache) {
-            return cache.addAll(
-                '/',
-                'js/main.js',
-                'css/style.css'
-            )
-        })
-    )
+    // event.waitUntil(
+    //     caches.open('cache-v1').then(function (cache) {
+    //         return cache.addAll(
+    //             '/',
+    //             'js/main.js',
+    //             'css/style.css'
+    //         )
+    //     })
+    // )
 })
 self.addEventListener('fetch', function (event) {
-    console.log(event.request);
-
+    console.log('hello');
+    event.respondWith(
+        new Response('lalalalalala', {
+            headers: { 'foo': 'bar'}
+        })
+    )
 })
